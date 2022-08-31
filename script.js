ea76cc6587557f2325ddb7de0eb5ac0391d1e012
Mohammed Niyas <niyask1997@gmail.com>
2022-08-31 17:33:35 +0530

    deleted the menu icon javascript function

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function myFunction() {
     var x = document.getElementById("topnav");
-    if (x.className === "nav-list") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav-list";
-    }
+    
   }
 
 
